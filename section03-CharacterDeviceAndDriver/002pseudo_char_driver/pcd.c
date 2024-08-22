9172911c25782a03b7b168186095a4da58031baa
Baris Can Coskun <bariscancoskun@hotmail.com>
2024-08-22 12:50:33 +0300

    Section 3:Char Dev and Driver finished

--- section03-CharacterDeviceAndDriver/002pseudo_char_driver/pcd.c
+++ section03-CharacterDeviceAndDriver/002pseudo_char_driver/pcd.c
@@ -1,7 +1,12 @@
 #include<linux/module.h>
 #include<linux/fs.h>
 #include<linux/cdev.h>
+#include<linux/device.h>
+#include<linux/kdev_t.h>
 //pcd : pseudo char driver
+
+#undef pr_fmt
+#define pr_fmt(fmt) "%s:" fmt, __func__
 
 #define DEV_MEM_SIZE 512
 
@@ -16,22 +21,27 @@ struct cdev pcd_cdev;
 
 //defining file operation methods
 loff_t pcd_lseek(struct file *filp, loff_t off, int whence){
+	pr_info("lseek requested\n");
 	return 0;
 }
 
 ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos){
+        pr_info("read requested for %zu bytes \n", count);
 	return 0;
 }
 
 ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos){
+        pr_info("write requested for %zu bytes \n", count);
 	return 0;
 }
 
 int pcd_open(struct inode *inode, struct file *filp){
+        pr_info("open was successful\n");
 	return 0;
 }
 
 int pcd_release(struct inode *inode, struct file *filp){
+        pr_info("close was successful\n");
 	return 0;
 }
 
@@ -45,23 +55,46 @@ struct file_operations pcd_fops ={
 	.owner = THIS_MODULE
 };
 
+struct class *class_pcd;
+
+struct device *device_pcd;
+
 static int __init pcd_driver_init(void){
 	//1. dynamically allocate a device number
-	alloc_chrdev_region(&device_number, 0, 1, "pcd");
+	alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+
+	pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
 
 	//2. Initialize the cdev structure with fops
-	//pcd_cdev.owner = THIS_MODULE;
+	pcd_cdev.owner = THIS_MODULE;
 	cdev_init(&pcd_cdev, &pcd_fops);
 
 	//3. Register a device (cdev structure) with VFS 
 	cdev_add(&pcd_cdev, device_number, 1);
 
+	//4. create device class under /sys/class/
+	class_pcd = class_create("pcd_class");
+
+	//5. device file creation
+	// populate the sysfs with device information
+	device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd");
+
+	pr_info("Module init was successful\n");
+
 	return 0;
 }
 
 static void __exit pcd_driver_cleanup(void){
+	//chronologically reverse order
+	device_destroy(class_pcd, device_number);
 
+	class_destroy(class_pcd);
 
+	cdev_del(&pcd_cdev);
+
+	unregister_chrdev_region(device_number, 1);
+
+	pr_info("module unloaded\n");
 }
 
 module_init(pcd_driver_init);
--- section03-CharacterDeviceAndDriver/002pseudo_char_driver/pcd.mod.c
+++ section03-CharacterDeviceAndDriver/002pseudo_char_driver/pcd.mod.c
@@ -1,10 +1,18 @@
 #include <linux/module.h>
 #define INCLUDE_VERMAGIC
 #include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
+#include <linux/export-internal.h>
 #include <linux/vermagic.h>
 #include <linux/compiler.h>
 
+#ifdef CONFIG_UNWINDER_ORC
+#include <asm/orc_header.h>
+ORC_HEADER;
+#endif
+
 BUILD_SALT;
+BUILD_LTO_INFO;
 
 MODULE_INFO(vermagic, VERMAGIC_STRING);
 MODULE_INFO(name, KBUILD_MODNAME);
@@ -23,14 +31,26 @@ __section(".gnu.linkonce.this_module") = {
 MODULE_INFO(retpoline, "Y");
 #endif
 
+
+
 static const struct modversion_info ____versions[]
 __used __section("__versions") = {
-	{ 0x3b12617d, "module_layout" },
-	{ 0x7cf5b970, "cdev_add" },
-	{ 0x818f4cbd, "cdev_init" },
+	{ 0x122c3a7e, "_printk" },
+	{ 0x5b8239ca, "__x86_return_thunk" },
 	{ 0xe3ec2f2b, "alloc_chrdev_region" },
-	{ 0xefd6cf06, "__aeabi_unwind_cpp_pr0" },
+	{ 0xf5f85346, "cdev_init" },
+	{ 0x63597d5f, "cdev_add" },
+	{ 0x1dee04a0, "class_create" },
+	{ 0x714f9a77, "device_create" },
+	{ 0xe8e5ec15, "device_destroy" },
+	{ 0x704fc943, "class_destroy" },
+	{ 0xcd643cea, "cdev_del" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0xbdfb6dbb, "__fentry__" },
+	{ 0x708cd699, "module_layout" },
 };
 
 MODULE_INFO(depends, "");
 
+
+MODULE_INFO(srcversion, "17D2CF9D0162F0DA004DA6B");
